fb7d9f0cae77b83f88b6d891a0b9ec5b12aa0f24
Steve Richert <steve.richert@gmail.com>
2011-12-13 16:44:39 -0500

    Parse gem method call options

--- lib/gemnasium/parser/patterns.rb
+++ lib/gemnasium/parser/patterns.rb
@@ -8,17 +8,33 @@ module Gemnasium
       REQUIREMENT = /\s*(?:#{MATCHER}\s*)?#{VERSION}\s*/
 
       KEY = /(?::\w+|:?"\w+"|:?'\w+')/
-      SYMBOL = /(?::\w+|:"[^"]+"|'[^']+')/
-      STRING = /(?:"[^"]*"|'[^']*')/
+      SYMBOL = /(?::\w+|:"[^"#]+"|'[^']+')/
+      STRING = /(?:"[^"#]*"|'[^']*')/
       BOOLEAN = /(?:true|false)/
       NIL = /nil/
       ELEMENT = /(?:#{SYMBOL}|#{STRING})/
       ARRAY = /\[(?:#{ELEMENT}(?:\s*,\s*#{ELEMENT})*)?\]/
       VALUE = /(?:#{BOOLEAN}|#{NIL}|#{ELEMENT}|#{ARRAY}|)/
-      PAIR = /(?:#{KEY}\s*=>\s*#{VALUE}|\w+:\s+#{VALUE})/
+      PAIR = /(?:(#{KEY})\s*=>\s*(#{VALUE})|(\w+):\s+(#{VALUE}))/
       OPTIONS = /#{PAIR}(?:\s*,\s*#{PAIR})*/
 
-      GEM_CALL = /^\s*gem\s+(?<q1>["'])(?<name>#{GEM_NAME})\k<q1>(?:\s*,\s*(?<q2>["'])(?<requirement_1>#{REQUIREMENT})\k<q2>(?:\s*,\s*(?<q3>["'])(?<requirement_2>#{REQUIREMENT})\k<q3>)?)?(?:\s*,\s*(?<options>#{OPTIONS})\s*)?$/
+      GEM_CALL = /^\s*gem\s+(?<q1>["'])(?<name>#{GEM_NAME})\k<q1>(?:\s*,\s*(?<q2>["'])(?<req1>#{REQUIREMENT})\k<q2>(?:\s*,\s*(?<q3>["'])(?<req2>#{REQUIREMENT})\k<q3>)?)?(?:\s*,\s*(?<opts>#{OPTIONS})\s*)?$/
+
+      def self.options(string)
+        return {} unless string
+        hash, raw = {}, Hash[*string.match(OPTIONS).captures.compact]
+        raw.each do |key, value|
+          new_key = key.tr(%(:"'), "").to_sym
+          new_value = case value
+          when BOOLEAN then value == "true"
+          when NIL then nil
+          when SYMBOL then value.tr(%(:"'), "").to_sym
+          when STRING then value.tr(%("'), "")
+          end
+          hash[new_key] = new_value
+        end
+        hash
+      end
     end
   end
 end
