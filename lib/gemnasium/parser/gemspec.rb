a3b790d17a2ca533f9e849852102e19904d70c1e
Steve Richert <steve.richert@gmail.com>
2011-12-14 07:18:36 -0500

    Pass the specs on Ruby 1.9.3

--- lib/gemnasium/parser/gemspec.rb
+++ lib/gemnasium/parser/gemspec.rb
@@ -1,6 +1,12 @@
 module Gemnasium
   module Parser
     class Gemspec
+      attr_reader :content
+
+      def initialize(content)
+        @content = content
+      end
+
       def dependencies
         raise NotImplementedError
       end
